35d12411d973fc0953dde5ae9033199329b5c77e
kid <nguyentinh215@gmail.com>
2023-11-13 09:07:37 +0700

    s

--- src/main/java/com/kid/helloworld/webservice/DemoApplication.java
+++ src/main/java/com/kid/helloworld/webservice/DemoApplication.java
@@ -1,9 +1,9 @@
 package com.kid.helloworld.webservice;
 
 // import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoApplication {
 
     // public static void main(String[] args) {
--- src/main/java/com/kid/helloworld/webservice/GreetingController.java
+++ src/main/java/com/kid/helloworld/webservice/GreetingController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+//@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+    // @GetMapping("/greeting")
+    // public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    //     return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    // }
 }
